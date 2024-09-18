d48d7f3207c1dc7cd19c113d72d0102005c54466
Kevin Gomes <kevingomes1211@gmail.com>
2024-09-18 16:57:19 +0530

    add readme.md, implement random name selection for city, localize initial full names vector from file to the name generation function only

--- city.h
+++ city.h
@@ -1,12 +1,16 @@
 #pragma once
+
 #include <string>
 #include<iostream>
 #include<cmath>
 #include<fstream>
-#include "route.h"
-#include<map>
 #include<vector>
-#include<time.h>
+#include<algorithm>
+#include<random>
+#include<map>
+
+#include "route.h"
+
 class City {
     public:
     std::string name;
@@ -22,23 +26,33 @@ class City {
 };
 
 namespace CityStatic{
-        std::vector<std::string> cityNames;
         City cities [10][10];
         int count = 0;
 
         
-        void getCityNames () {
+        std::vector<std::string> getCityNames (int total) {
+
+            std::vector<std::string> tempCityNames;
             std::ifstream input ("citynames.txt");
+
             for ( std::string line ; std::getline( input, line ) ; ) {
-                cityNames.push_back(line);
+                tempCityNames.push_back(line);
             }
+
+            std::random_device rd;
+            std::default_random_engine engine(rd()); 
+
+            std::shuffle(tempCityNames.begin(), tempCityNames.end(), engine);
+
+            std::vector<std::string> cityNames(tempCityNames.begin(), tempCityNames.begin() + total);
+
+            return cityNames;
         }
 
         void generateCity(int total) {
-            srand(time(0));
             int sqrtTotal = std::sqrt(total);
 
-            getCityNames();
+            std::vector<std::string> cityNames = getCityNames(total);
 
             for (int i=0; i < sqrtTotal ; i++) { 
             
@@ -46,7 +60,7 @@ namespace CityStatic{
                     std::cout << count;
                     std::cout << " : " << i << j << " : " << cityNames[count] << std::endl;
                     
-                    cities[i][j] = City(cityNames[count], count); // generate city, store it in matrix, give it a name from names arr n pass it "count" as the id
+                    cities[i][j] = City(cityNames[count], count); // generate city, store it in matrix
 
                     if ( i - 1 >= 0 ) {
                         // make routes top
@@ -63,6 +77,5 @@ namespace CityStatic{
                 }
 
             }
-            cityNames.clear();
         }
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+# About
+a game i'm making in C++ to learn the langauge better. i already made one called dumbgeon about a year ago but i wanted to make something more complex. i was specifically inspired by dwarf fortress' world generation and wanted to make a world where you're a participant but clearly the world has lived before you and will live after you. you are merely interacting with a pre existing world, that's what i wanted to make. and i thought this would be a proper challenge that should teach me enoguh about C++. i know there's certain aspects of C++ i still won't interact with but this should get me pretty good with the basics at least :p
+
+the gameplay itself is inspired by tradewinds, a game about visiting different ports, buying and selling things and fighting pirates! pretty fun. i'm making an encapsulated version of it with less of a focus on gameplay and story, and more on the world itself. let's see how that goes!
+
+as of now, i'm not planning on making the game have an interface other than the terminal itself. you'll have to type in commands and/or just type the choice number to navigate the UI. unfortunately, this will be how you see this beautiful world i'm trying to make. quite nice though, isn't it? you can only use simpe terminal inputs and yet you're interacting with this crazy in depth world? i thought it's quite cool! ... but yeah maybe i'll try to make a TUI if nothing else. i really don't wnat to get bogged down by aspects that aren't absolutely essential (i mean how useful is learning how to make a UI in c++ anyway?)
+
+# issues 
+none yet thankfully! (there probably are i just don't know them yet so none yet)
+
+# plans
+the todo.md file has future plans listed! whether it be immediate plans or plans for the future, possibly ones i'll leave out in this game and add in a sequel/spinoff
